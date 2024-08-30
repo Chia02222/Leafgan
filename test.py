a10e2f2078d32e9f6f4aadf14403eb0ed0e9a7e8
Chia0222 <130571713+Chia0222@users.noreply.github.com>
2024-08-30 17:17:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,42 +1,26 @@
-"""General-purpose test script for image-to-image translation.
-
-Once you have trained your model with train.py, you can use this script to test the model.
-It will load a saved model from --checkpoints_dir and save the results to --results_dir.
-
-It first creates model and dataset given the option. It will hard-code some parameters.
-It then runs inference for --num_test images and save results to an HTML file.
-
-Example (You need to train models first or download pre-trained models from our website):
-    Test a CycleGAN model (both sides):
-        python test.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
-
-    Test a CycleGAN model (one side only):
-        python test.py --dataroot datasets/horse2zebra/testA --name horse2zebra_pretrained --model test --no_dropout
-
-    The option '--model test' is used for generating CycleGAN results only for one side.
-    This option will automatically set '--dataset_mode single', which only loads the images from one set.
-    On the contrary, using '--model cycle_gan' requires loading and generating results in both directions,
-    which is sometimes unnecessary. The results will be saved at ./results/.
-    Use '--results_dir <directory_path_to_save_result>' to specify the results directory.
-
-    Test a pix2pix model:
-        python test.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
-
-See options/base_options.py and options/test_options.py for more test options.
-See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
-See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
-"""
 import os
 from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
 from util.visualizer import save_images
 from util import html
+import torch
+import numpy as np
+from skimage.metrics import peak_signal_noise_ratio as compare_psnr
+from skimage.metrics import mean_squared_error as compare_mse
+import csv
 
+def calculate_rmse(imageA, imageB):
+    mse = compare_mse(imageA, imageB)
+    rmse = np.sqrt(mse)
+    return rmse
+
+def calculate_psnr(imageA, imageB):
+    psnr = compare_psnr(imageA, imageB)
+    return psnr
 
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
-    # hard-code some parameters for test
     opt.num_threads = 0   # test code only supports num_threads = 1
     opt.batch_size = 1    # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
@@ -45,14 +29,19 @@ if __name__ == '__main__':
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
-    # create a website
+    
     web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.epoch))  # define the website directory
     webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
-    # test with eval mode. This only affects layers like batchnorm and dropout.
-    # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
-    # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
+
+    # Initialize CSV file to save metrics
+    metrics_file = os.path.join(web_dir, 'metrics.csv')
+    with open(metrics_file, mode='w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Image', 'RMSE', 'PSNR'])
+
     if opt.eval:
         model.eval()
+
     for i, data in enumerate(dataset):
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
@@ -62,5 +51,23 @@ if __name__ == '__main__':
         img_path = model.get_image_paths()     # get image paths
         if i % 5 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
+        
+        # Convert tensors to numpy arrays for RMSE and PSNR calculation
+        real_A = visuals['real_A'].cpu().numpy().transpose(1, 2, 0)  # Convert tensor to numpy
+        fake_B = visuals['fake_B'].cpu().numpy().transpose(1, 2, 0)  # Convert tensor to numpy
+
+        # Calculate RMSE and PSNR
+        rmse_value = calculate_rmse(real_A, fake_B)
+        psnr_value = calculate_psnr(real_A, fake_B)
+
+        print(f'Image {i}: RMSE = {rmse_value}, PSNR = {psnr_value}')
+        
+        # Save metrics to CSV
+        with open(metrics_file, mode='a') as file:
+            writer = csv.writer(file)
+            writer.writerow([img_path[0], rmse_value, psnr_value])
+        
+        # Save the images
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
+    
     webpage.save()  # save the HTML
