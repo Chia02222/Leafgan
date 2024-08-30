17a3e5e252315ab1d9a7af39a6bf650960292d5a
Chia0222 <130571713+Chia0222@users.noreply.github.com>
2024-08-30 22:33:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ import torch
 import matplotlib.pyplot as plt
 import os
 import csv
-import os
-from options.test_options import TestOptions
+from options.test_options import TestOptions  # Assuming you have this module
 from data import create_dataset
 from models import create_model
 from util.visualizer import save_images
@@ -25,6 +24,7 @@ def calculate_psnr(real_images, reconstructed_images):
     mse = torch.nn.functional.mse_loss(real_images, reconstructed_images)
     psnr = 10 * torch.log10(1 / mse)
     return psnr.item()
+
 def save_metrics_plot(epoch_mse_A, epoch_psnr_A, epoch_mse_B, epoch_psnr_B, checkpoint_dir):
     plt.figure()
     plt.subplot(2, 2, 1)
@@ -62,18 +62,17 @@ def save_metrics_csv(epoch_mse_A, epoch_psnr_A, epoch_mse_B, epoch_psnr_B, epoch
         writer.writerow(['Epoch', 'Average MSE A', 'Average PSNR A', 'Average MSE B', 'Average PSNR B', 'Loss'])
         for epoch in range(len(epoch_mse_A)):
             writer.writerow([epoch + 1, epoch_mse_A[epoch], epoch_psnr_A[epoch], epoch_mse_B[epoch], epoch_psnr_B[epoch], epoch_losses[epoch]])
-            
+
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
-    # hard-code some parameters for test
-    opt.num_threads = 0   # test code only supports num_threads = 1
-    opt.batch_size = 1    # test code only supports batch_size = 1
-    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
-    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
-    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
-    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    model = create_model(opt)      # create a model given opt.model and other options
-    model.setup(opt)               # regular setup: load and print networks; create schedulers
+    opt.num_threads = 0
+    opt.batch_size = 1
+    opt.serial_batches = True
+    opt.no_flip = True
+    opt.display_id = -1
+    dataset = create_dataset(opt)
+    model = create_model(opt)
+    model.setup(opt)
 
     mse_list_A = []
     psnr_list_A = []
@@ -84,83 +83,66 @@ if __name__ == '__main__':
     epoch_mse_B = []
     epoch_psnr_B = []
     epoch_losses = []
-    
-    # create a website
-    web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.epoch))  # define the website directory
+
+    web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.epoch))
     webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
-    # test with eval mode. This only affects layers like batchnorm and dropout.
-    # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
-    # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
+
     if opt.eval:
         model.eval()
+
     for i, data in enumerate(dataset):
         iter_start_time = time.time()
-            if total_iters % opt.print_freq == 0:
-                t_data = iter_start_time - iter_data_time
-            visualizer.reset()
-            total_iters += opt.batch_size
-            epoch_iter += opt.batch_size
-            model.set_input(data)
-            model.optimize_parameters()
+        model.set_input(data)
+        model.optimize_parameters()
 
-            real_A = data['A'].to(model.device)
-            real_B = data['B'].to(model.device)
-            visuals = model.get_current_visuals()
+        real_A = data['A'].to(model.device)
+        real_B = data['B'].to(model.device)
+        visuals = model.get_current_visuals()
 
-            rec_A_key = 'rec_A'
-            rec_B_key = 'rec_B'
-            if rec_A_key in visuals:
-                rec_A = visuals[rec_A_key].to(model.device)
+        if 'rec_A' in visuals:
+            rec_A = visuals['rec_A'].to(model.device)
+            mse_A = calculate_mse(real_A, rec_A)
+            psnr_A = calculate_psnr(real_A, rec_A)
+            mse_list_A.append(mse_A)
+            psnr_list_A.append(psnr_A)
 
-                mse_A = calculate_mse(real_A, rec_A)
-                psnr_A = calculate_psnr(real_A, rec_A)
-
-                mse_list_A.append(mse_A)
-                psnr_list_A.append(psnr_A)
-
-                if total_iters % opt.print_freq == 0:
-                    print(f'MSE A: {mse_A}')
-                    print(f'PSNR A: {psnr_A}')
-
-            if rec_B_key in visuals:
-                rec_B = visuals[rec_B_key].to(model.device)
-
-                mse_B = calculate_mse(real_B, rec_B)
-                psnr_B = calculate_psnr(real_B, rec_B)
-
-                mse_list_B.append(mse_B)
-                psnr_list_B.append(psnr_B)
-
-                if total_iters % opt.print_freq == 0:
-                    print(f'MSE B: {mse_B}')
-                    print(f'PSNR B: {psnr_B}')
+        if 'rec_B' in visuals:
+            rec_B = visuals['rec_B'].to(model.device)
+            mse_B = calculate_mse(real_B, rec_B)
+            psnr_B = calculate_psnr(real_B, rec_B)
+            mse_list_B.append(mse_B)
+            psnr_list_B.append(psnr_B)
 
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
-        model.set_input(data)  # unpack data from data loader
-        model.test()           # run inference
-        visuals = model.get_current_visuals()  # get image results
-        img_path = model.get_image_paths()     # get image paths
-        if i % 5 == 0:  # save images to an HTML file
+
+        model.test()
+        visuals = model.get_current_visuals()
+        img_path = model.get_image_paths()
+        if i % 5 == 0:
             print('processing (%04d)-th image... %s' % (i, img_path))
 
         # Calculate and store average metrics for the epoch
-            avg_mse_A = np.mean(mse_list_A)
-            avg_psnr_A = np.mean(psnr_list_A)
-            avg_mse_B = np.mean(mse_list_B)
-            avg_psnr_B = np.mean(psnr_list_B)
-            avg_loss = np.mean([losses[k] for k in losses])  # Calculate average loss
-            epoch_mse_A.append(avg_mse_A)
-            epoch_psnr_A.append(avg_psnr_A)
-            epoch_mse_B.append(avg_mse_B)
-            epoch_psnr_B.append(avg_psnr_B)
-            epoch_losses.append(avg_loss)
+        avg_mse_A = np.mean(mse_list_A)
+        avg_psnr_A = np.mean(psnr_list_A)
+        avg_mse_B = np.mean(mse_list_B)
+        avg_psnr_B = np.mean(psnr_list_B)
+        avg_loss = np.mean(model.get_current_losses().values())  # Assuming this returns a dict
+        epoch_mse_A.append(avg_mse_A)
+        epoch_psnr_A.append(avg_psnr_A)
+        epoch_mse_B.append(avg_mse_B)
+        epoch_psnr_B.append(avg_psnr_B)
+        epoch_losses.append(avg_loss)
 
-            print(f'Epoch: {epoch}, Average MSE A: {avg_mse_A:.4f}, Average PSNR A: {avg_psnr_A:.4f}')
-            print(f'Epoch: {epoch}, Average MSE B: {avg_mse_B:.4f}, Average PSNR B: {avg_psnr_B:.4f}')
-            
+        print(f'Epoch: {i}, Average MSE A: {avg_mse_A:.4f}, Average PSNR A: {avg_psnr_A:.4f}')
+        print(f'Epoch: {i}, Average MSE B: {avg_mse_B:.4f}, Average PSNR B: {avg_psnr_B:.4f}')
+
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
-        save_metrics_plot(epoch_mse_A, epoch_psnr_A, epoch_mse_B, epoch_psnr_B, checkpoint_dir)
-        save_metrics_csv(epoch_mse_A, epoch_psnr_A, epoch_mse_B, epoch_psnr_B, epoch_losses, checkpoint_dir)
 
-    webpage.save()  # save the HTML
+    # After looping through the dataset
+    checkpoint_dir = os.path.join(opt.results_dir, 'checkpoints')
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    save_metrics_plot(epoch_mse_A, epoch_psnr_A, epoch_mse_B, epoch_psnr_B, checkpoint_dir)
+    save_metrics_csv(epoch_mse_A, epoch_psnr_A, epoch_mse_B, epoch_psnr_B, epoch_losses, checkpoint_dir)
+
+    webpage.save()
