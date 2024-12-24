284aee08ddb5c2cdbe8ec546d4b77d15494120b2
Chia0222 <130571713+Chia0222@users.noreply.github.com>
2024-12-24 14:18:20 +0800

    Update train_SSIM.py

--- train_SSIM.py
+++ train_SSIM.py
@@ -28,7 +28,7 @@ def calculate_fid(real_images, reconstructed_images, transform, batch_size=8):
 
     # Define the transformation pipeline for FID computation
     transform = transforms.Compose([
-        transforms.Resize((256, 256)),  # Resize to a smaller size
+        transforms.Resize((224, 224)),  # Resize to a smaller size
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
