1496708f350800a689427c9a79de9ba06b5eb06b
Chia0222 <130571713+Chia0222@users.noreply.github.com>
2024-12-26 16:07:14 +0800

    Update train_SSIM1.py

--- train_SSIM1.py
+++ train_SSIM1.py
@@ -9,67 +9,10 @@ from options.train_options import TrainOptions
 from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
-from scipy.linalg import sqrtm
 from skimage.metrics import structural_similarity as ssim
 from sklearn.decomposition import PCA
 from torchvision import transforms  # Add this import for transforms
 from PIL import Image
-
-# Define transformation pipeline
-transform = transforms.Compose([
-    transforms.Resize((256, 256)),  # Resize images to a fixed size
-    transforms.ToTensor(),          # Convert to Tensor
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize to [-1, 1]
-])
-
-def calculate_fid(real_images, reconstructed_images, transform, batch_size=8, pca_components=5):
-    device = real_images.device
-    real_images = real_images.to(device)
-    reconstructed_images = reconstructed_images.to(device)
-
-    if len(real_images) < 10 or len(reconstructed_images) < 10:
-        print("Not enough samples for FID calculation. Need at least 10.")
-        return 0
-
-    real_features = []
-    reconstructed_features = []
-
-    for i in range(0, len(real_images), batch_size):
-        batch_real = real_images[i:i + batch_size]
-        batch_rec = reconstructed_images[i:i + batch_size]
-
-        batch_real_features = []
-        batch_rec_features = []
-
-        for img_real, img_rec in zip(batch_real, batch_rec):
-            img_real_pil = Image.fromarray((img_real.permute(1, 2, 0).cpu().numpy() * 255).astype('uint8'))
-            img_rec_pil = Image.fromarray((img_rec.permute(1, 2, 0).cpu().numpy() * 255).astype('uint8'))
-
-            img_real_transformed = transform(img_real_pil).unsqueeze(0).to(device)
-            img_rec_transformed = transform(img_rec_pil).unsqueeze(0).to(device)
-
-            batch_real_features.append(img_real_transformed)
-            batch_rec_features.append(img_rec_transformed)
-
-        real_features.extend(batch_real_features)
-        reconstructed_features.extend(batch_rec_features)
-
-    real_features = torch.cat(real_features, dim=0).view(-1, real_features[0].numel()).cpu().numpy()
-    reconstructed_features = torch.cat(reconstructed_features, dim=0).view(-1, reconstructed_features[0].numel()).cpu().numpy()
-
-    pca = PCA(n_components=pca_components)
-    real_features_pca = pca.fit_transform(real_features)
-    reconstructed_features_pca = pca.transform(reconstructed_features)
-
-    mu1, sigma1 = np.mean(real_features_pca, axis=0), np.cov(real_features_pca, rowvar=False)
-    mu2, sigma2 = np.mean(reconstructed_features_pca, axis=0), np.cov(reconstructed_features_pca, rowvar=False)
-
-    epsilon = 1e-6
-    sigma1 += epsilon * np.eye(sigma1.shape[0])
-    sigma2 += epsilon * np.eye(sigma2.shape[0])
-
-    fid = np.sum((mu1 - mu2) ** 2) + np.trace(sigma1 + sigma2 - 2 * sqrtm(sigma1 @ sigma2))
-    return fid
 
 def calculate_ssim(real_image, reconstructed_image):
     real_image = real_image.detach().cpu().numpy().transpose(1, 2, 0)
@@ -82,13 +25,21 @@ def calculate_ssim(real_image, reconstructed_image):
     reconstructed_gray = cv2.cvtColor(reconstructed_image, cv2.COLOR_RGB2GRAY)
 
     return ssim(real_gray, reconstructed_gray)
-
-def save_metrics_plot(epoch_fid_A, epoch_ssim_A, epoch_fid_B, epoch_ssim_B, checkpoint_dir):
+    
+def calculate_psnr(real_images, reconstructed_images):
+    device = real_images.device
+    real_images = real_images.to(device)
+    reconstructed_images = reconstructed_images.to(device)
+    mse = torch.nn.functional.mse_loss(real_images, reconstructed_images)
+    psnr = 10 * torch.log10(1 / mse)
+    return psnr.item()
+    
+def save_metrics_plot(epoch_psnr_A, epoch_ssim_A, epoch_psnr_B, epoch_ssim_B,epoch_losses, checkpoint_dir):
     plt.figure()
     plt.subplot(2, 2, 1)
-    plt.plot(range(len(epoch_fid_A)), epoch_fid_A, label='Average FID A')
+    plt.plot(range(len(epoch_psnr_A)), epoch_psnr_A, label='Average PSNR A')
     plt.xlabel('Epoch')
-    plt.ylabel('Average FID A')
+    plt.ylabel('Average PSNR A')
     plt.legend()
 
     plt.subplot(2, 2, 2)
@@ -98,9 +49,9 @@ def save_metrics_plot(epoch_fid_A, epoch_ssim_A, epoch_fid_B, epoch_ssim_B, chec
     plt.legend()
 
     plt.subplot(2, 2, 3)
-    plt.plot(range(len(epoch_fid_B)), epoch_fid_B, label='Average FID B')
+    plt.plot(range(len(epoch_psnr_B)), epoch_psnr_B, label='Average psnr B')
     plt.xlabel('Epoch')
-    plt.ylabel('Average FID B')
+    plt.ylabel('Average PSNR B')
     plt.legend()
 
     plt.subplot(2, 2, 4)
@@ -112,14 +63,24 @@ def save_metrics_plot(epoch_fid_A, epoch_ssim_A, epoch_fid_B, epoch_ssim_B, chec
     plt.tight_layout()
     plt.savefig(os.path.join(checkpoint_dir, 'metrics_plot.png'))
     plt.close()
+    
+    # Plot epoch losses
+    plt.figure()
+    plt.plot(range(len(epoch_losses)), epoch_losses, label='Epoch Losses')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(os.path.join(checkpoint_dir, 'loss_plot.png'))
+    plt.close()
 
-def save_metrics_csv(epoch_fid_A, epoch_ssim_A, epoch_fid_B, epoch_ssim_B, epoch_losses, checkpoint_dir):
+def save_metrics_csv(epoch_psnr_A, epoch_ssim_A, epoch_psnr_B, epoch_ssim_B, epoch_losses, checkpoint_dir):
     csv_file = os.path.join(checkpoint_dir, 'metrics.csv')
     with open(csv_file, 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Epoch', 'Average FID A', 'Average SSIM A', 'Average FID B', 'Average SSIM B', 'Loss'])
+        writer.writerow(['Epoch', 'Average PSNR A', 'Average SSIM A', 'Average PSNR B', 'Average SSIM B', 'Loss'])
         for epoch in range(len(epoch_fid_A)):
-            writer.writerow([epoch + 1, epoch_fid_A[epoch], epoch_ssim_A[epoch], epoch_fid_B[epoch], epoch_ssim_B[epoch], epoch_losses[epoch]])
+            writer.writerow([epoch + 1, epoch_psnr_A[epoch], epoch_ssim_A[epoch], epoch_psnr_B[epoch], epoch_ssim_B[epoch], epoch_losses[epoch]])
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()
@@ -131,6 +92,9 @@ if __name__ == '__main__':
     model.setup(opt)
     visualizer = Visualizer(opt)
     total_iters = 0
+
+    psnr_list_A, ssim_list_A, psnr_list_B, ssim_list_B, epoch_losses = [], [], [], [], []
+    
     checkpoint_dir = 'checkpoints'
     os.makedirs(checkpoint_dir, exist_ok=True)
 
@@ -138,8 +102,6 @@ if __name__ == '__main__':
         epoch_start_time = time.time()
         iter_data_time = time.time()
         epoch_iter = 0
-
-        fid_list_A, ssim_list_A, fid_list_B, ssim_list_B, epoch_losses = [], [], [], [], []
 
         for i, data in enumerate(dataset):
             iter_start_time = time.time()
@@ -150,6 +112,49 @@ if __name__ == '__main__':
             epoch_iter += opt.batch_size
             model.set_input(data)
             model.optimize_parameters()
+
+            model.compute_visuals()
+            visuals = model.get_current_visuals()
+            real_A = data['A'].to(model.device)
+            rec_A = visuals['rec_A'].to(model.device)
+            real_B = data['B'].to(model.device)
+            rec_B = visuals['rec_B'].to(model.device)
+
+            if rec_A_key in visuals:
+                rec_A = visuals[rec_A_key].to(model.device)
+                ssim_A = calculate_ssim(real_A[0], rec_A[0])
+                ssim_list_A.append(ssim_A)
+                psnr_A = calculate_psnr(real_A, rec_A)
+                psnr_list_A.append(psnr_A)
+                
+                # Track best metrics for A
+                if ssim_A > best_ssim_A:
+                    best_ssim_A = ssim_A
+                if psnr_A > best_psnr_A:
+                    best_psnr_A = psnr_A
+
+                if total_iters % opt.print_freq == 0:
+                    print(f'SSIM A: {ssim_A}')
+                    print(f'PSNR A: {psnr_A}')
+                    
+
+            if rec_B_key in visuals:
+                rec_B = visuals[rec_B_key].to(model.device)
+                ssim_B = calculate_ssim(real_B[0], rec_B[0])
+                ssim_list_B.append(ssim_B)
+                psnr_B = calculate_psnr(real_B, rec_B)
+                psnr_list_B.append(psnr_B)
+
+                # Track best metrics for B
+                if ssim_B > best_ssim_B:
+                    best_ssim_B = ssim_B
+                if psnr_B > best_psnr_B:
+                    best_psnr_B = psnr_B
+
+                if total_iters % opt.print_freq == 0:
+                    print(f'SSIM B: {ssim_B}')
+                    print(f'PSNR B: {psnr_B}')
+
 
             if total_iters % opt.display_freq == 0:
                 save_result = total_iters % opt.update_html_freq == 0
@@ -168,33 +173,9 @@ if __name__ == '__main__':
                 save_suffix = f'iter_{total_iters}' if opt.save_by_iter else 'latest'
                 model.save_networks(save_suffix)
 
-
             iter_data_time = time.time()
 
-        model.compute_visuals()
-        visuals = model.get_current_visuals()
-        real_A = data['A'].to(model.device)
-        rec_A = visuals['rec_A'].to(model.device)
-        real_B = data['B'].to(model.device)
-        rec_B = visuals['rec_B'].to(model.device)
-
-        ssim_A = calculate_ssim(real_A[0], rec_A[0])
-        ssim_B = calculate_ssim(real_B[0], rec_B[0])
-        ssim_list_A.append(ssim_A)
-        ssim_list_B.append(ssim_B)
-
-        avg_loss = np.mean([losses[k] for k in losses])
-        epoch_losses.append(avg_loss)
-
         print(f'Epoch {epoch} - SSIM A: {ssim_A} - SSIM B: {ssim_B}')
-
-        if epoch % opt.fid_interval == 0:
-            fid_A = calculate_fid(real_A, rec_A, transform)
-            fid_B = calculate_fid(real_B, rec_B, transform)
-            fid_list_A.append(fid_A)
-            fid_list_B.append(fid_B)
-
-            print(f'Epoch {epoch} - FID A: {fid_A} - FID B: {fid_B}')
             
         if epoch % opt.save_epoch_freq == 0:
             print(f'Saving the model at the end of epoch {epoch}, iters {total_iters}')
@@ -203,16 +184,36 @@ if __name__ == '__main__':
             
             avg_ssim_A = np.mean(ssim_list_A) if ssim_list_A else None
             avg_ssim_B = np.mean(ssim_list_B) if ssim_list_B else None
-            avg_fid_A = np.mean(fid_list_A) if fid_list_A else None
-            avg_fid_B = np.mean(fid_list_B) if fid_list_B else None
+            avg_psnr_A = np.mean(psnr_list_A) if psnr_list_A else None
+            avg_psnr_B = np.mean(psnr_list_B) if psnr_list_B else None
+            avg_loss = np.mean([losses[k] for k in losses])
+            epoch_losses.append(avg_loss)
+            epoch_ssim_A.append(avg_ssim_A)
+            epoch_psnr_A.append(avg_psnr_A)
+            epoch_ssim_B.append(avg_ssim_B)
+            epoch_psnr_B.append(avg_psnr_B)
+            epoch_losses.append(avg_loss)
+
+            final_mse_A = avg_mse_A
+            final_psnr_A = avg_psnr_A
+            final_mse_B = avg_mse_B
+            final_psnr_B = avg_psnr_B
 
             print(f'Epoch {epoch} - Average SSIM A: {avg_ssim_A} - Average SSIM B: {avg_ssim_B}')
-            print(f'Epoch {epoch} - Average FID A: {avg_fid_A} - Average FID B: {avg_fid_B}')
+            print(f'Epoch {epoch} - Average PSNR A: {avg_psnr_A} - Average PSNR B: {avg_psnr_B}')
 
         epoch_end_time = time.time()
         print(f'Epoch {epoch} completed in {epoch_end_time - epoch_start_time:.2f} seconds.')
 
-    save_metrics_plot(fid_list_A, ssim_list_A, fid_list_B, ssim_list_B, checkpoint_dir)
+    save_metrics_plot(fid_list_A, ssim_list_A, fid_list_B, ssim_list_B,epoch_looses, checkpoint_dir)
     save_metrics_csv(fid_list_A, ssim_list_A, fid_list_B, ssim_list_B, epoch_losses, checkpoint_dir)
-    
+
+    # Save best and final metrics
+    with open(os.path.join(opt.checkpoints_dir, 'best_and_final_metrics.txt'), 'w') as f:
+        f.write(f'Best PSNR A: {best_psnr_A:.4f}, Best PSNR A: {best_psnr_A:.4f}\n')
+        f.write(f'Best PSNR B: {best_psnr_B:.4f}, Best PSNR B: {best_psnr_B:.4f}\n')
+        f.write(f'Final PSNR A: {final_psnr_A:.4f}, Final PSNR A: {final_psnr_A:.4f}\n')
+        f.write(f'Final PSNR B: {final_psnr_B:.4f}, Final PSNR B: {final_psnr_B:.4f}\n')
+
+    model.update_learning_rate()
     model.update_learning_rate()
